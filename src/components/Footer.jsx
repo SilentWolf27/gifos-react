aa554bf06016712d9de90cde76cc12fd37464873
Silent <97pedro@live.com.mx>
2022-05-22 17:25:53 -0500

    Footer responsivo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import "@styles/components/Footer.scss"
+import "@styles/components/Footer.scss";
 
 const Footer = () => {
   return (
     <footer>
-      <p>Compartir en:</p>
-      <div className="social-container">
-        <span className="social-icon facebook"></span>
-        <span className="social-icon twitter"></span>
-        <span className="social-icon instagram"></span>
+      <div className="social-divider">
+        <p>Compartir en:</p>
+        <div className="social-container">
+          <span className="social-icon facebook"></span>
+          <span className="social-icon twitter"></span>
+          <span className="social-icon instagram"></span>
+        </div>
       </div>
       <p className="copyright">Gifos 2022 All Rights Reserved.</p>
     </footer>
