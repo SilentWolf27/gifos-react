4f0af4f6183eeb8d67ac3d6b0e69672af5161f97
Silent <97pedro@live.com.mx>
2022-06-07 22:03:13 -0500

    diseño slider md y superior

--- src/containers/GifSlider.jsx
+++ src/containers/GifSlider.jsx
@@ -5,8 +5,12 @@ import { getTrendingGifs } from "@utils/Giphy";
 
 const GifSlider = () => {
     const [gifs, setGifs] = useState([]);
-    const [offset, setoffset] = useState(0);
-    const limit = 9;
+    const [offset, setOffset] = useState(0);
+    const limit = 3;
+    const [isBigScreen, setIsBigScreen] = useState(false);
+
+    const mediaWatcher = window.matchMedia("(min-width: 900px)");
+    mediaWatcher.addEventListener("change", (e) => setIsBigScreen(e.matches));
 
     useEffect(() => {
         getTrendingGifs(offset, limit)
@@ -16,17 +20,25 @@ const GifSlider = () => {
             .catch((error) => {});
     }, []);
 
+    useEffect(() => {
+        setIsBigScreen(mediaWatcher.matches);
+    }, [isBigScreen]);
+
     return (
-        <div className="slider">
-            {gifs.map((item) => (
-                <Gif
-                    key={item.id}
-                    src={item.images.fixed_height.url}
-                    title={item.title}
-                    url={item.url}
-                    username={item.username}
-                />
-            ))}
+        <div className="slider-container">
+            {isBigScreen && <span className="slider-button left"></span>}
+            <div className="slider">
+                {gifs.map((item) => (
+                    <Gif
+                        key={item.id}
+                        src={item.images.fixed_height.url}
+                        title={item.title}
+                        url={item.url}
+                        username={item.username}
+                    />
+                ))}
+            </div>
+            {isBigScreen && <span className="slider-button right"></span>}
         </div>
     );
 };
